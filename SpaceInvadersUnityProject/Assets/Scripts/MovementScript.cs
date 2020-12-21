33c6232970666f97e5bb8b43b766e6faac1459d8
Sashking <sashking.cz@gmail.com>
2020-12-21 12:57:22 +0100

    Added test movement script + added rocket sprite

---
+++ SpaceInvadersUnityProject/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            var transform1 = transform;
+            var position = transform1.position;
+            position.x -= speed;
+            transform1.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            var transform1 = transform;
+            var position = transform1.position;
+            position.x += speed;
+            transform1.position = position;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            var transform1 = transform;
+            var position = transform1.position;
+            position.y += speed;
+            transform1.position = position;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            var transform1 = transform;
+            var position = transform1.position;
+            position.y -= speed;
+            transform1.position = position;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.Equals(""))
+        {
+            
+        }
+    }
+}
