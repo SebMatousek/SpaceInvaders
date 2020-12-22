20192d6ec94bf251958e0c8a7b67ed2368593631
SebMatousek <seb.matousek@gmail.com>
2020-12-22 12:17:53 +0100

    no message

--- SpaceInvadersUnityProject/Assets/Weapon.cs
+++ SpaceInvadersUnityProject/Assets/Weapon.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
+    public GameObject bulletPrefab;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetKeyDown(KeyCode))
         {
             Shoot();
             Debug.Log("pew");
@@ -18,6 +19,6 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
-
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
