39fb8134eb65d4da8df7723bfd3061835064158d
SebMatousek <seb.matousek@gmail.com>
2020-12-23 10:52:00 +0100

    Made enemy, collider for rocket. Enemy finish added. TODO: End of game if crashed, if enemy crossed finish

---
+++ SpaceInvadersUnityProject/Assets/EnemyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField] private float enemySpeed = 0.5f;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb.velocity = -transform.up * enemySpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitObject)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ SpaceInvadersUnityProject/Assets/Spawning.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public Transform SpawnPoint;
+    [SerializeField] private float spawnChancePercent = 10;
+    [SerializeField] private GameObject enemyPrefab;
+    private float enemyX;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Random.Range(0.0f, 1.0f) < (spawnChancePercent / 2000))
+        {
+            enemyX = Random.Range(-15f, 15f);
+            Debug.Log("Spawn enemy at: " + enemyX);
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemyPrefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
+    }
+}
