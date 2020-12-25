95c3b1e353a3b4b60a48f6676d00f5200164c60c
SebMatousek <seb.matousek@gmail.com>
2020-12-25 18:13:46 +0100

    Revert "Made four types of enemies. TODO: Enemies don't kill each other"
    This reverts commit ae8ffa2863e1d28951d34f7ab7c65575fe8a4a11.

--- SpaceInvadersUnityProject/Assets/Spawning.cs
+++ SpaceInvadersUnityProject/Assets/Spawning.cs
@@ -6,17 +6,13 @@ public class Spawning : MonoBehaviour
 {
     public Transform SpawnPoint;
     [SerializeField] private float spawnChancePercent = 10;
-    [SerializeField] private GameObject enemy1Prefab;
-    [SerializeField] private GameObject enemy2Prefab;
-    [SerializeField] private GameObject enemy3Prefab;
-    [SerializeField] private GameObject enemy4Prefab;
+    [SerializeField] private GameObject enemyPrefab;
     private float enemyX;
-    private float spawnKoeficient = 1500;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Random.Range(0.0f, 1.0f) < (spawnChancePercent / spawnKoeficient))
+        if (Random.Range(0.0f, 1.0f) < (spawnChancePercent / 2000))
         {
             enemyX = Random.Range(-15f, 15f);
             Debug.Log("Spawn enemy at: " + enemyX);
@@ -26,23 +22,6 @@ public class Spawning : MonoBehaviour
 
     void Spawn()
     {
-        int enemyNum = Random.Range(0, 100);
-
-        if (enemyNum < 40)
-        {
-            Instantiate(enemy1Prefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
-        }
-        else if (enemyNum < 75)
-        {
-            Instantiate(enemy2Prefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
-        }
-        else if (enemyNum < 90)
-        {
-            Instantiate(enemy3Prefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
-        }
-        else
-        {
-            Instantiate(enemy4Prefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
-        }
+        Instantiate(enemyPrefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
     }
 }
