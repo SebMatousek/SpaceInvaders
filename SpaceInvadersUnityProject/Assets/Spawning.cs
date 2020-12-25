ae8ffa2863e1d28951d34f7ab7c65575fe8a4a11
SebMatousek <seb.matousek@gmail.com>
2020-12-25 18:02:50 +0100

    Made four types of enemies. TODO: Enemies don't kill each other

--- SpaceInvadersUnityProject/Assets/Spawning.cs
+++ SpaceInvadersUnityProject/Assets/Spawning.cs
@@ -6,13 +6,17 @@ public class Spawning : MonoBehaviour
 {
     public Transform SpawnPoint;
     [SerializeField] private float spawnChancePercent = 10;
-    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject enemy1Prefab;
+    [SerializeField] private GameObject enemy2Prefab;
+    [SerializeField] private GameObject enemy3Prefab;
+    [SerializeField] private GameObject enemy4Prefab;
     private float enemyX;
+    private float spawnKoeficient = 1500;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Random.Range(0.0f, 1.0f) < (spawnChancePercent / 2000))
+        if (Random.Range(0.0f, 1.0f) < (spawnChancePercent / spawnKoeficient))
         {
             enemyX = Random.Range(-15f, 15f);
             Debug.Log("Spawn enemy at: " + enemyX);
@@ -22,6 +26,23 @@ public class Spawning : MonoBehaviour
 
     void Spawn()
     {
-        Instantiate(enemyPrefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
+        int enemyNum = Random.Range(0, 100);
+
+        if (enemyNum < 40)
+        {
+            Instantiate(enemy1Prefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
+        }
+        else if (enemyNum < 75)
+        {
+            Instantiate(enemy2Prefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
+        }
+        else if (enemyNum < 90)
+        {
+            Instantiate(enemy3Prefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(enemy4Prefab, new Vector3(enemyX, SpawnPoint.transform.position.y, 1), Quaternion.identity);
+        }
     }
 }
